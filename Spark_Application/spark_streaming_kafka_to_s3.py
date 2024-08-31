7676b57a67d806670e69883275f795d39a34e074
Ra99it <thdwpals8975@naver.com>
2024-08-31 16:49:35 +0900

    refactor: 편의성을 위한 코드 변경 (Kafka 서버 외부 입력, S3 저장 시 날짜를 기준)
    Kafka 서버는 보안 상 외부에서 입력하도록 변경하였습니다. 그리고 데이터가 저장되는 위치와 checkpoint의 가독성을 위해서 오늘 날자를 기준 디렉토리를 생성하고 그 안에 데이터를 생성합니다. 또한 코드 컨벤션을 적용하였고, 유저명 뿐만 아니라 IP 정보도 수집하지 않도록 변경하였습니다.

--- Spark_Application/spark_streaming_kafka_to_s3.py
+++ Spark_Application/spark_streaming_kafka_to_s3.py
@@ -1,9 +1,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json
-from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType
 from pyspark.sql.functions import sha2
 import os
 import sys
+from datetime import datetime
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
@@ -16,21 +17,24 @@ spark = SparkSession.builder.appName("League Of Legend Player Streaming") \
   .enableHiveSupport() \
   .getOrCreate()
 
+kafka_bootstrap_server = spark.conf.get("spark.kafka.bootstrap.servers")
+kafka_topic = spark.conf.get("spark.kafka.topic")
+
 kafkaStream = spark \
   .readStream \
   .format("kafka") \
-  .option("kafka.bootstrap.servers", "<ip>:<port>") \
-  .option("subscribe", "<topic>") \
+  .option("kafka.bootstrap.servers", kafka_bootstrap_server) \
+  .option("subscribe", kafka_topic) \
   .load()
 
 schema = StructType([
-  StructField("createRoomDate", StringType(), True),
+  StructField("createRoomDate", TimestampType(), True),
   StructField("method", StringType(), True),
   StructField("ingametime", StringType(), True),
   StructField("ip", StringType(), True),
   StructField("deathCount", StringType(), True),
   StructField("roomID", StringType(), True),
-  StructField("datetime", StringType(), True),
+  StructField("datetime", TimestampType(), True),
   StructField("x", StringType(), True),
   StructField("y", StringType(), True),
   StructField("inputkey", StringType(), True),
@@ -43,19 +47,48 @@ jsonParsedStream = kafkaStream.selectExpr("CAST(value As STRING)") \
   .select(from_json(col("value"), schema).alias("data")) \
   .select("data.*")
 
-jsonParsedStream = jsonParsedStream.withColumn("account_sha2", sha2(col("account"), 256))
-jsonParsedStream = jsonParsedStream.drop("account")
+jsonParsedStream = jsonParsedStream \
+  .withColumn("create_room_date", col("createRoomDate")) \
+  .withColumn("current_time", col("ingametime")) \
+  .withColumn("death_count", col("deathCount")) \
+  .withColumn("room_id", col("roomID")) \
+  .withColumn("death_count", col("createRoomDate").cast("int")) \
+  .withColumn("x", col("x").cast("int")) \
+  .withColumn("y", col("y").cast("int")) \
+  .withColumn("status", col("status").cast("int")) \
+  .drop("createRoomDate").drop("ingametime").drop("deathCount").drop("roomID")
 
-transformedPlayerLogs = jsonParsedStream.toDF("createRoomDate", "method", "ingametime", "ip",
-                           "deathCount", "roomID", "datetime", "x", "y",
-                           "inputkey", "champion", "status", "account_sha2")
+jsonParsedStream = jsonParsedStream \
+  .withColumn("encrypted_account", sha2(col("account"), 256)) \
+  .drop("account") \
+  .drop("ip")
+
+jsonParsedStream = jsonParsedStream.select("create_room_date",
+                                           "method",
+                                           "current_time",
+                                           "death_count",
+                                           "room_id",
+                                           "datetime",
+                                           "encrypted_account",
+                                           "x",
+                                           "y",
+                                           "inputkey",
+                                           "champion",
+                                           "status")
+
+transformedPlayerLogs = jsonParsedStream.toDF("create_room_date", "method",
+                                              "current_time", "death_count",
+                                              "room_id", "datetime",
+                                              "encrypted_account", "x", "y",
+                                              "inputkey", "champion", "status")
+
 
 playerLogsStreamWriter = transformedPlayerLogs.writeStream \
   .trigger(processingTime='1 minute') \
   .outputMode("append") \
   .format("json") \
-  .option("path", "s3://sjm-simple-data/bronzelayer/playerlogs/") \
-  .option("checkpointLocation", "s3://sjm-simple-data/checkpoint/Riot/") \
+  .option("path", "s3://sjm-simple-data/bronzelayer/" + str(datetime.today().strftime("%Y-%m-%d"))) \
+  .option("checkpointLocation","s3://sjm-simple-data/checkpoint/" + str(datetime.today().strftime("%Y-%m-%d"))) \
   .queryName("query_playerLogs") \
   .start()
 
