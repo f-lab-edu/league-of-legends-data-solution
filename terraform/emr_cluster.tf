5894cceccd77ec586249cfed79a2b7876c840d8f
Ra99it <thdwpals8975@naver.com>
2024-09-02 07:00:02 +0900

    feat: IaC도구인 Terraform으로 AWS EMR 인프라 관리 및 프로비저닝

---
+++ terraform/emr_cluster.tf
@@ -0,0 +1,66 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_emr_cluster" "cluster" {
+  name = aws_cluster.name
+  release_label = "emr-6.8.0"
+  applications = ["Spark", "Hadoop", "JupyterHub", "Trino", "Zeppelin"]
+  log_uri = aws_s3.bucket
+
+  service_role = aws_iam_role.iam_emr_service_role.arn
+
+  unhealthy_node_replacement = false
+  termination_protection = false
+  keep_job_flow_alive_when_no_steps = true
+
+  ec2_attributes {
+    subnet_id                         = aws_subnet.main.id
+    emr_managed_master_security_group = aws_security_group.sg.id
+    emr_managed_slave_security_group  = aws_security_group.sg.id
+    instance_profile                  = aws_iam_instance_profile.emr_profile.arn
+    key_name = aws_key
+  }
+
+  master_instance_group {
+    instance_type = "m5.xlarge"
+    instance_count = 1
+    ebs_config {
+      size = "32"
+      type = "gp2"
+      volumes_per_instance = 2
+    }
+  }
+
+  core_instance_group {
+    instance_type = "m5.xlarge"
+    instance_count = 1
+    ebs_config {
+      size = "32"
+      type = "gp2"
+      volumes_per_instance = 2
+    }
+  }
+
+  configurations_json = <<EOF
+  [
+    {
+      "Classification" : "hive-site",
+      "Properties" : {
+         "hive.metastore.client.factory.class" : "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"
+      },
+       "Configurations" : []
+    },
+    {
+      "Classification": "spark-hive-site",
+      "Properties" : {
+        "hive.metastore.client.factory.class": "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"
+      },
+      "Configurations" : []
+    }
+  ]
+EOF
+
+  scale_down_behavior = "TERMINATE_AT_TASK_COMPLETION"
+  ebs_root_volume_size = "30"
+}
