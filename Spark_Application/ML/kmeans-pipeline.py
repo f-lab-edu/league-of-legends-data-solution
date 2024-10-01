6051b48b85084cc01670be2b0efbfc5c2a70befe
Ra99it <thdwpals8975@naver.com>
2024-10-01 17:46:51 +0900

    feat: KMeans Pipeline 구현

---
+++ Spark_Application/ML/kmeans-pipeline.py
@@ -0,0 +1,60 @@
+from pyspark.sql import SparkSession
+
+from pyspark.sql.functions import col
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.clustering import KMeans
+from pyspark.ml import Pipeline
+from pyspark.ml.evaluation import ClusteringEvaluator
+import os
+import sys
+
+os.environ["PYSPARK_PYTHON"] = sys.executable
+os.environ["PYSPARK_DRIVER_PYTHON"] = sys.executable
+
+spark = (
+  SparkSession.builder.appName("ML-Pipeline-Kmeans")
+  .master("yarn")
+  .config("spark.home", "/usr/lib/spark")
+  .config(
+      "spark.jars",
+      "/usr/share/aws/delta/lib/delta-core.jar,/usr/share/aws/delta/lib/delta-storage.jar,/usr/share/aws/delta/lib/delta-storage-s3-dynamodb.jar",
+  )
+  .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+  .config(
+      "spark.sql.catalog.spark_catalog",
+      "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+  )
+  .enableHiveSupport()
+  .getOrCreate()
+)
+
+filter_time = sys.argv[1]
+
+# Silver Layer Train Data 가져오기
+train_data = spark.table("silver_train_riot.delta_playerlogs").filter(
+    col("create_room_date") == filter_time
+)
+
+# Feature Vector 만들기
+assembler = VectorAssembler(
+    inputCols=["x", "y", "distance"], outputCol="features_vector"
+)
+
+# KMeans 설정
+kmeans = KMeans(k=2, seed=1, featuresCol="features_vector",
+                predictionCol="prediction")
+
+pipeline = Pipeline(stages=[assembler, kmeans])
+
+pipelineModel = pipeline.fit(train_data)
+train_data_prediction = pipelineModel.transform(train_data)
+
+evaluator = ClusteringEvaluator(
+    featuresCol="features_vector", predictionCol="prediction",
+    metricName="silhouette"
+)
+silhouette = evaluator.evaluate(train_data_prediction)
+
+print(f"Score: {silhouette}")
+
+pipelineModel.write().overwrite().save("s3://sjm-simple-data/app/model_tmp/")
